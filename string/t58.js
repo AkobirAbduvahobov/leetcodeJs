0012138a2c06aa983ac70a17b1d30a0eab7d96fe
AkobirAbduvahobov <akobirabduvahobov@gmail.com>
2023-12-19 16:59:31 +0500

    hop

---
+++ string/t58.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) 
+{
+    
+    
+    var counter = 0;
+    var i = 0;
+    var a = 5;
+    while( true )
+    {
+        if( s[s.length-1 - i] !== ' ' )
+        {
+            a = 4;
+        }
+        if( a == 4 ) 
+        {
+            ++counter;
+            if( i == s.length ) return  counter-1;
+            if( s[s.length-1 - i] === ' ' )
+            {
+                return counter-1;
+            }
+        }
+        ++i;
+    }
+    return counter-1;
+};
+
+var test1 = "Salom dunyosa   ";
+var res = lengthOfLastWord(test1);
+console.log(res);
